eaea58208b5e3b06e4e59e7730e172f3f80221c7
BathTubTheGreat <yilyanel@nycstudents.net>
2026-03-27 10:04:33 -0400

    rjyrjrtjjttje

--- app.py
+++ app.py
@@ -1,20 +1,69 @@
 class pet():
-    def __init__(self,name,money,health,happiness,satiety):
+    def __init__(self,name,money,inventory,happiness,satiety,poo,health,cleanliness):
         self.name = name
         self.money = money
-        self.health = health
+        self.inventory = inventory
         self.happiness = happiness
         self.satiety = satiety
+        self.poo = poo
+        self.health = health
+        self.cleanliness = cleanliness
 
-    def putitdown(self,health):
-        ptdwn = input("doy ou want to put your pet down? (yes/no) ")
-        if ptdwn == "yes":
-            ogpet.health = 0
-            print("your pet", ogpet.name, "is DEAD ")
+    def buy(self, item):
+        self.inventory.append(item)
+        
+        print("this is what you have in your inventory:")
+        for i in self.inventory:
+            print(i["name"])
+        print("you have", self.money, "dollars left")
 
-    def feedit
+    def feed(self):
+        self.satiety += 100
+        self.happiness +=10
+        self.poo +=100
+        print("your pet,", self.name, "is fed to", self.satiety, "satiety, but he needs to poop", self.poo, "%")
+    
+    def poop(self):
+        self.satiety = 0
+        print(self.name, "has pooped, and he is at", self.poo ("% poop"))
+        self.cleanliness +=50
 
-newpet = input("make a new pet (name) ")
-ogpet = pet(newpet, 0, 100, 50, 70)
+    def accident(self):
+        self.satiety = 0
+        print(self.name, "has pooped all over your carpet, and he is at", self.poo, ("% poop"))
+        self.cleanliness -= 60
+    
+    def clean(self):
+        self.cleanliness = 100
+        print("you have cleaned the house from its poop")
+    def noclean(self):
+        self.cleanliness = 0
+        self.health = 0
+        print(self.name, "has contracted salmonella and")
+        
+Poopyguy = pet("poopguy", 20, [{"name": "poop"}],50,0,0,100,100)
 
-pet.putitdown(ogpet,100)
+while Poopyguy.health > 0:
+
+    buy = input("do you want to buy somethign (yes/no)")
+    if buy == "yes":
+        Poopyguy.buy({"name": "stick", "atk": 2, "price": 20})
+
+    fed = input("do you want to give it food (yes/no) ")
+    if fed == "yes":
+        Poopyguy.feed()
+
+    pooo = input("do you want to take him to poop (yes/no) ")
+    if pooo == "yes":
+        Poopyguy.poop()
+    elif pooo == "no":
+        Poopyguy.accident()
+        clean = input("do you want to clean it and the house from tis poop? (yes/no) ")
+        if clean == "yes":
+            Poopyguy.clean
+        if clean == "no":
+            Poopyguy.noclean
+
+if Poopyguy.health <=0:
+    print(Poopyguy.name, "is dead")
+
