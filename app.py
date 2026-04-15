d3a8e2a99a8461a76c7bccc50c86c7c619737e3c
BathTubTheGreat <yilyanel@nycstudents.net>
2026-04-15 10:16:47 -0400

    bvojsghbviwrghbwskhbwfkwkhvbswghbwgknwqg

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-class pet():
-    def __init__(self,name,money,inventory,happiness,satiety,poo,health,cleanliness):
+
+import random
+
+class pet(): 
+    def __init__(self,name,money,inventory,happiness,satiety,poo,health,cleanliness,strength):
         self.name = name
         self.money = money
         self.inventory = inventory
@@ -8,6 +11,7 @@ class pet():
         self.poo = poo
         self.health = health
         self.cleanliness = cleanliness
+        self.strength = strength
 
     def buy(self, item):
         self.inventory.append(item)
@@ -24,47 +28,72 @@ class pet():
         print("your pet,", self.name, "is fed to", self.satiety, "satiety, but he needs to poop", self.poo, "%")
     
     def poop(self):
-        self.satiety = 0
-        print(self.name, "has pooped, and he is at", self.poo ("% poop"))
+        self.poo = 0
+        print(self.name, "has pooped, and he is at", self.poo, ("% poop"))
         self.cleanliness +=50
 
     def accident(self):
-        self.satiety = 0
+        self.poo = 0
         print(self.name, "has pooped all over your carpet, and he is at", self.poo, ("% poop"))
         self.cleanliness -= 60
     
     def clean(self):
-        self.cleanliness = 100
+        self.cleanliness +=70
         print("you have cleaned the house from its poop")
     def noclean(self):
         self.cleanliness = 0
         self.health = 0
-        print(self.name, "has contracted salmonella and")
+        print(self.name, "has contracted salmonella")
     
     def putdown(self):
         self.health = 0
     
     def display_info(self):
-        print(f"Name: {self.name}, Money: {self.money}, Happiness: {self.happiness}, Satiety: {self.satiety}, Poop meter: {self.poo}, health: {self.health}, cleanliness {self.cleanliness}")
+        print("Name:", self.name, "Money:", self.money, "Happiness:", self.happiness, "Satiety:", self.satiety, "Poop meter:", self.poo, "health:", self.health, "cleanliness", self.cleanliness)
     
     def play(self):
         self.happiness +=50
         print("wow your pet", self.name, "is now at", self.happiness, "happiness")
-           
-Poopyguy = pet("poopguy", 20, [{"name": "poop"}],50,0,0,100,100)
+    
+    def train(self):
+        self.strength +=4
+        print("your pet is now at", self.strength, "strength")
+
+class attacker():
+    def __init__(self,attack,health):
+        self.attack = attack
+        self.health = health
+    def attac(self):
+        Poopyguy.health -= self.attack
+        print("the attacker has attacked your pet, and he is now at", Poopyguy.health, "health")
+    def showstat(self):
+        print("health:", self.health, "attack:", self.attack)
+
+Poopyguy = pet("poopguy", 20, [{"name": "poop"}],50,50,50,100,100,0)
+attackerer = attacker(random.uniform(1,10),random.uniform(1,100))
 
 while Poopyguy.health > 0:
+
+    stating = input("do you want to check your pets stats? (yes/no) ")
+    if stating == "yes":
+        Poopyguy.display_info()
 
     buy = input("do you want to buy somethign (yes/no)")
     if buy == "yes":
         Poopyguy.buy({"name": "stick", "atk": 2, "price": 20})
 
+    play = input(f"do you want to play with {Poopyguy.name}")
+    if play == "yes":
+        Poopyguy.play()
+
     fed = input("do you want to give it food (yes/no) ")
     if fed == "yes":
         Poopyguy.feed()
+
     if Poopyguy.poo > 100:
                 Poopyguy.health -= 2
                 print("yo poopyguy is dying because he has too much poop in his butt")
+
     if Poopyguy.poo >50:
         pooo = input("do you want to take him to poop (yes/no) ")
         if pooo == "yes":
@@ -76,15 +105,38 @@ while Poopyguy.health > 0:
                 Poopyguy.clean()
             if clean == "no":
                 Poopyguy.noclean()
+
+    if Poopyguy.satiety <=0:
+        Poopyguy.health -= 5
+        print("you need to feed poopyguy he is dying ")
+
+    traininging = input("do you want to train your pet ")
+    if traininging == "yes":
+        Poopyguy.train
     putdowning = input("do you want to put its poor life down")
     if putdowning == "yes":
         Poopyguy.putdown()
-    stating = input("do you want to check your pets stats? (yes/no) ")
-    if stating == "yes":
-        Poopyguy.display_info()
-if Poopyguy.satiety <=0:
-    Poopyguy.health -= 5
-    print("you need to feed poopyguy he is dying ")
+
+    Poopyguy.satiety -=5
+    Poopyguy.happiness -=5
+    battle = random.uniform(1,10)
+
+    if battle == 6:
+        while attackerer.health > 0:
+            fart = 1*(1+(Poopyguy.strength/100))
+            slash = 4*(1+(Poopyguy.strength/100))
+            punch = 3*(1+(Poopyguy.strength/100))
+            print("oh no another animal is attacking your pet:")
+            attackerer.showstat()
+            attackerer.attac()
+            atick = input("what do you want to hit him with? (fart,slash,punch)")
+            if atick == fart:
+                attackerer.health -= fart
+            elif atick ==slash:
+                attackerer.health -= fart
+                bleed = True
+        if attackerer.health < 0:
+
 if Poopyguy.health <=0:
     print(Poopyguy.name, "is dead")
 if Poopyguy.happiness < 0:
